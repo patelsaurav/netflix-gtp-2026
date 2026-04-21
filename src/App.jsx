f977f6c279c44807071a4b154d30cc72e96a30be
patelsaurav <patelsaurav1333@gmail.com>
2026-04-21 23:46:30 +0530

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from './assets/vite.svg'
+import heroImg from './assets/hero.png'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <h1 className="text-4xl font-bold underline text-blue-600 p-4">
+        Hello World
+      </h1>
+    </>
+  )
+}
+
+export default App
